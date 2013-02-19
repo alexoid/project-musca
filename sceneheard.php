daa48e7199db25e24ec54e5568c10bb41992802a
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-20 00:26:23 +0200

    Simple Layout Page

--- sceneheard.php
+++ sceneheard.php
@@ -1,49 +1,6 @@
-<!doctype html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]> <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]> <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]> <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-
-    <title>SAY Quarterly</title>
-
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-
-    <meta property="og:type" content="article"/>
-    <meta property="og:image" content="/share.png"/>
-    <meta property="og:site_name" content=""/>
-    <meta property="og:url" content=""/>
-    <meta property="og:title" content=""/>
-    <meta property="og:description" content=""/>
-
-    <link href="/favicon.png" rel="shortcut icon" type="image/png" />
-    <link href="/share.png" rel="image_src" type="image/png" />
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.0.4/modernizr.min.js"></script>
-    <script type="text/javascript" src="//use.typekit.net/ynl1uag.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-    <script src="_js/jquery.isotope.min.js"></script>
-    <script src="_js/scripts.js"></script>
-    <!-- <script src="_js/jquery.freetile.min.js"></script> -->
-
-    <style type="text/css" media="screen">
-        @import url("//grid.ohmylovely.com/1/24/0/672/960/1200/");
-        @import url("_css/styles.css");
-        @import url("_assets/sceneheard/styles.css");
-    </style>
-
-</head>
-
-<body>
-
-<?php include "_inc/header.php"; ?>
+<style type="text/css" media="screen">
+    @import url("_assets/sceneheard/styles.css");
+</style>
 
 <section class="hero">
     <div class="lovely-grid">
@@ -87,9 +44,4 @@
         </ul>
     </div>
 </section>
-
-</body>
-
-</html>
-
 
