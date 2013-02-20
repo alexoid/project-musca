9d8b39c76ed7f1d30cbf6323d3e39e2c0facc764
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-20 23:23:53 +0200

    Savoring Article

---
+++ article-savoring.php
@@ -0,0 +1,63 @@
+<style type="text/css" media="screen">
+    @import url("_assets/savoring-auth/styles.css");
+</style>
+
+<section class="hero"><div class="diags">
+    <div class="lovely-grid">
+        <div class="grid-24">
+            <div class="wrapper">
+                <div class="bug hide-mobile"></div>
+                <h3>Brand Case Study</h3>
+                <h2 class="hide-mobile">Kate Upton Takes<br/> Zoo York</h2>
+                <h2 class="small remove-672 remove-960 remove-1200">Kate Upton Takes<br/> Zoo York</h2>
+            </div>
+        </div>
+    </div>
+</div></section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-5 pull-2 pad-mobile-20">
+            rail
+        </div>
+        <div class="grid-10 pull-1 push-2 pad-mobile-20">
+            <p class="first">A supermodel, plucky roaches and an award-winning campaign take an apparel line to the digital advertising hall of fame.</p>
+            <p>Zoo York apparel embodies the attitude and lifestyle of the Big Apple’s skateboarding, graffiti and hip-hop community. To showcase its brand, the company put together an irreverent and engaging interactive experience, highlighting the clothing company’s latest styles.</p>
+            <p>Starring supermodel and actress Kate Upton and a couple of cockroaches with attitude, the campaign included videos, rich media and social sharing. Created on <a href="#">Say Media’s AdFrames 3 platform</a>, the ad unit featured a series of “choose your own ending” videos starring Upton as well as an embedded “smash-a-roach” challenge and a “lookbook” of the company’s current line.</p>
+        </div>
+        <div class="grid-2 push-2 pad-mobile-20 hide-mobile">
+            <img src="_img/social.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-5 pull-2 pad-mobile-20">
+            <div class="photo">
+                <img src="_assets/zoo-york/video.png" alt="">
+                <p class="caption hide-mobile">
+                    See it in action
+                    <span>0:15 sec</span>
+                </p>
+            </div>
+        </div>
+        <div class="grid-14 pull-1 push-2 pad-mobile-20">
+            <div class="photo">
+                <div class="fence"></div>
+                <img src="_assets/zoo-york/photo-01.png" alt="">
+                <p class="caption right block">The ad drove three and half times the average click-through rate</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 push-6 pad-mobile-20">
+            <p>Customers could select from several customized endings to the video story, ensuring that they would spend time with the experience. Meanwhile, roaches crawling in and out of the screen were a hit with the target demographic. With its interactive interface, celebrity star power and rich media, the ad drove high engagement — three and half times the benchmark average click-thru rate.</p>
+            <p>The popular and well-designed campaign garnered a prestigious OMMA Award for Online Advertising Creativity, which honors advertisers that push the potential of online advertising creative. Unfortunately, we didn’t capture high scores on roach-smashing, but it definitely added to the fun.</p>
+        </div>
+    </div>
+</section>
