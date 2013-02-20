e6c8cdaf7e5998eaef52b81f4f993a6657072708
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-21 01:37:40 +0200

    Canon Article

---
+++ canon.php
@@ -0,0 +1,59 @@
+<style type="text/css" media="screen">
+    @import url("_assets/canon/styles.css");
+</style>
+
+<section class="hero"><div class="grads">
+    <div class="lovely-grid">
+        <div class="grid-24">
+            <div class="wrapper">
+                <div class="bug hide-mobile"></div>
+                <h3>Brand Case Study</h3>
+                <h2 class="hide-mobile">Behind the lens</h2>
+                <h2 class="small remove-672 remove-960 remove-1200">Behind the lens</h2>
+                <h3>Gear Patrol X Canon</h3>
+            </div>
+        </div>
+    </div>
+</div></section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-5 pull-2 pad-mobile-20">
+            rail
+
+            <div class="photo">
+                <img src="_assets/canon/video.png" alt="">
+                <p class="caption hide-mobile">
+                    See it in action
+                    <span>0:15 sec</span>
+                </p>
+            </div>
+
+        </div>
+        <div class="grid-14 push-2 pull-1 pad-mobile-20">
+
+            <div class="grid-10 push-2 nested-first">
+                <p class="first">Canon and Say Media created an original content series around one editor's passion for photography.</p>
+            </div>
+
+            <div class="grid-2 pad-mobile-20 hide-mobile nested-last">
+                <img src="_img/social.png" alt="">
+            </div>
+
+
+
+            <div class="grid-7 nested-first">
+                <p>With the advent of smartphones and apps such as Instagram, photography instantly became much more accessible than ever before. Suddenly everyone had a camera in hand (or pocket) at all times and documenting the minute details of life became the status quo. But for many, the camera phone or the point-and-shoot just weren’t enough. Not everyone was willing to sacrifice quality for convenience.</p>
+                <p>To reach those discerning consumers, Canon partnered with Say Media and Gear Patrol to create a campaign to show aspiring photographers that, in fact, there’s no need to sacrifice quality or convenience. Canon wanted to demonstrate that DSLR cameras are easier to use than ever — while also teaching consumers how to shoot smarter and better.</p>
+                <p>Gear Patrol was the perfect site to help Canon’s campaign come into focus. A significant portion of the site’s readers are both novice and experienced photographers who trust the Gear Patrol editors for tips and advice on what to buy and why. Gear Patrol captured readers’ attention by providing them with knowledge about DSLR cameras and lenses as well as a series on how to most effectively shoot using Canon DSLRs in different situations, including urban locations and at night.</p>
+                <p>The posts lived on Gear Patrol’s site alongside high-impact, interactive ad units targeted to Say Media’s extensive network. As a result, photographers of all types were able to learn more about Canon’s DSLR offerings while also learning more about how to be a better photographer. Ultimately, the entire experience felt less like an ad and more like valuable content. An engaging photo gallery created by the editors increased time spent on the site relative to the consumer electronics benchmark.</p>
+                <p>A successful ad that benefits both the client and the consumer? Now that’s a picture worth capturing.</p>
+            </div>
+
+            <div class="grid-7 nested-last">
+                <img src="_assets/canon/eric.jpg">
+            </div>
+
+        </div>
+    </div>
+</section>
