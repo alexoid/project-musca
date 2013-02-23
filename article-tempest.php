3a761f8994e30c670aecfa87875c1bc4e649a49a
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-23 18:48:05 +0200

    Tempest (Unfinished)

---
+++ article-tempest.php
@@ -0,0 +1,29 @@
+<style type="text/css" media="screen">
+    @import url("_assets/tempest/styles.css");
+</style>
+
+<section class="hero"><div class="wrapper">
+    <div class="lovely-grid">
+        <div class="grid-24">
+            <h3>Innovation</h3>
+            <h2 class="hide-mobile">Meet Tempest<br/> Our Next-Generation<br/> Media Platform</h2>
+            <h2 class="small remove-672 remove-960 remove-1200">Meet Tempest<br/> Our Next-Generation<br/> Media Platform</h2>
+            <h3>By David Lerman</h3>
+        </div>
+        <div class="grid-24 tempest">
+            <div class="video"></div>
+        </div>
+    </div>
+</div></section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 push-2 pad-mobile-20">
+            <p class="first">Beautiful, scalable online media experiences have been limited by existing, cobbled-together systems. That’s about to change.</p>
+            <p>To quote the great computer scientist Alan Kay, the best way to predict the future is to invent it — so as part of our goal to build the media company of the future, we’ve also been building the technology platform to enable it. This initiative is broader than just developing a better content management system. It’s about looking at where media is headed and building a whole new kind of technology solution that dramatically changes the way media is created and consumed.</p>
+        </div>
+        <div class="grid-2 push-2 pad-mobile-20 hide-mobile">
+            <img src="_img/social.png" alt="">
+        </div>
+    </div>
+</section>
