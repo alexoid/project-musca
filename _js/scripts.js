adb3442e8c1fe352d5c425d4f65a1b6d7da4f295
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-24 18:02:43 +0200

    Tempest Updates

--- _js/scripts.js
+++ _js/scripts.js
@@ -2,16 +2,6 @@ $(document).ready(function($) {
 
     $('items').isotope({ layoutMode : 'masonry' });
 
-    $('.tempest .video').videoBG({
-        mp4:'_assets/tempest/tempest.mp4',
-        position:"absolute",
-        zIndex:500,
-        scale: true,
-        autoplay: true,
-        loop: true,
-        opacity: 1
-    });
-
 });
 
 // EOF
--- article-tempest.php
+++ article-tempest.php
@@ -10,8 +10,8 @@
             <h2 class="small remove-672 remove-960 remove-1200">Meet Tempest<br/> Our Next-Generation<br/> Media Platform</h2>
             <h3>By David Lerman</h3>
         </div>
-        <div class="grid-24 tempest">
-            <div class="video"></div>
+        <div class="grid-24">
+            <video src="_assets/tempest/tempest.mp4" autoplay="true" loop="true" />
         </div>
     </div>
 </div></section>
@@ -24,6 +24,43 @@
         </div>
         <div class="grid-2 push-2 pad-mobile-20 hide-mobile">
             <img src="_img/social.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 push-6 pad-mobile-20">
+            <p>Today’s digital media experiences are limited by the cobbled-together publishing systems they’re built with. The publication of the future won’t have a mobile template and a Web template; it will be built from the ground up for all devices. It will not have social plug-ins; it will be inherently social, richly interactive and deeply community-oriented. It won’t have Google Analytics or Chartbeat sitting on the side; it will integrate actionable data across every dimension of the experience to help build and grow audience. It won’t have ads tacked on in little boxes around the edges; it will deeply integrate brands into the site experience in a way that creates real value for the marketer while respecting (and even enhancing) the site’s voice. And most of all, the publication of the future will be authentic, it will be engaging and it will be beautiful.</p>
+            <p>Tempest is Say Media’s next-generation media platform that is making all this possible. Tempest publications seamlessly integrate content, community, commerce and marketing into a beautiful experience that is led by point of view and powered by great technology.</p>
+            <p>Tempest is not fundamentally a new initiative or a new product. We have been building the components of Tempest for years: better authoring tools, better site experiences and a better approach to advertising. Tempest is more a new way of thinking about how everything we build fits together into a cohesive whole and a vision of how that platform will evolve for the future. </p>
+            <p>Tempest is made of 8 components which we believe are the key elements of a great digital publishing experience.</p>
+            <p>Despite the breakneck pace of change on the Web, digital publishing hasn’t changed much in the last 10 years. We’re still stuck with the standard two-column layout — the article on the left, ads crowded on the right, social buttons on top, a commenting widget below. </p>
+         </div>
+    </div>
+</section>
+
+<section class="content fullwidth">
+
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 pad-mobile-20">
+            <p>Compare flipping through a beautiful, carefully laid-out print magazine to clicking through the modern content site; we’ve lost all sense of design, beauty and craft.</p>
+            <p><strong>Tempest Experience</strong> is our answer to this — a framework for building better digital media experiences. A Tempest publication is built from the ground up for all devices and seamlessly integrates community, commerce and marketing. It’s also beautiful, engaging and optimized.</p>
+            <p>This front-end experience is in turn powered by the four major pillars of the Tempest platform: Content, Community, Commerce and Marketing.</p>
+            <p><strong>Tempest Content</strong> provides powerful content management tools for the professional author and editor. Today’s common content management systems evolved mostly out of blogging and self-publishing systems, and while they’ve evolved tremendously, they’re still far from professional-class tools. A skilled publisher or editor needs a much more sophisticated toolset to create richer content, manage a budget and contributor pool, publish from anywhere, syndicate content through multiple channels, and leverage data to create better content and make better editorial decisions. Tempest Content focuses on providing easy-to-use tools that make professional authors and editors more effective.</p>
+            <p><strong>Tempest Community</strong> focuses on how a reader engages with editors, fellow readers and the brand voice as a whole. Great content publications bring together groups of like-minded individuals with a common interest. Tempest Community will give these groups new ways to interact with the content they’re passionate about and the people who share their passion.</p>
+            <p><strong>Tempest Commerce</strong> lets editors extend their point of view all the way to the shopping cart. With an ever-increasing number of options for what we buy and where we buy it, users increasingly need filters to find the best products. Content sites with trusted voices provide a perfect filter; I would much rather look at the 10 lighting fixtures Remodelista recommends than the 10,000 available on Amazon. The data backs this up; when our writers mention products in their posts, the products sell. Commerce is a huge opportunity to create value for the readers while giving publications another way to monetize.</p>
+            <p><strong>Tempest Marketing</strong> brings advertisers into the publication. Say Media has led the market in helping advertisers understand that standard banner ads are not working, and we will continue to provide them with better ways to engage readers with authenticity, accountability and scale. We’ll do this by helping them create great, authentic content that’s deeply integrated into our publications and helping them efficiently engage a broader audience for that content through our network. These four pillars of Tempest Content (content, community, commerce and marketing) are powered by a set of shared horizontal services: Distribution, Data and Infrastructure.</p>
+            <p><strong>Tempest Distribution</strong> provides the tools and services for growing audience, optimizing search traffic, leveraging social platforms such as Twitter, Facebook and Pinterest, and building syndication relationships with other sites that carry our content and drive audience to us.</p>
+            <p><strong>Tempest Data</strong> serves as the data warehouse for the whole platform, collecting and aggregating hundreds of terabytes of interaction data as users consume content, and providing actionable analytics across each of our services to help our publications more efficiently grow and engage their audiences.</p>
+            <p>And finally, <strong>Tempest Infrastructure</strong> provides all the underlying platform services that let us efficiently scale our business, and innovate rapidly.</p>
+            <p>Tempest is a work in progress, and we have miles to go to realize this vision in full — but we’re certain it’s the future of media and excited about the opportunities ahead.</p>
+        </div>
+        <div class="grid-5 pull-1">
+            <img src="_assets/tempest/tempest-modules.png">
         </div>
     </div>
 </section>
