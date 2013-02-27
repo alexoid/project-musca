bd220684806cc2604ca647af734c970788e7b92f
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-02-28 00:02:31 +0200

    xoVain

--- _inc/header.php
+++ _inc/header.php
@@ -4,7 +4,7 @@
             <ul class="tools">
                 <li><a href="#" title="View Contents"><i class="icon-reorder"></i></a></li>
             </ul>
-            <h1 class="logo small remove-672 remove-960 remove-1200">
+            <h1 class="logo small remove-768 remove-960 remove-1200">
                 <a href="#" title="SAY Quarterly">SAY Quarterly</a>
             </h1>
         </div>
@@ -13,7 +13,7 @@
                 <a href="#" title="Say Quarterly">Say Quarterly</a>
             </h1>
         </div>
-        <div class="grid-7 hide-mobile hide-408">
+        <div class="grid-7 hide-mobile">
             <p class="issue text-right">
                 Issue V &middot; Spring 2013
             </p>
---
+++ article-xovain.php
@@ -0,0 +1,71 @@
+<style type="text/css" media="screen">
+    @import url("_assets/xovain/styles.css");
+</style>
+
+<section class="hero">
+    <div class="wrapper">
+        <div class="lovely-grid">
+            <div class="grid-12 pull-6 push-6 pad-mobile-20">
+                <img src="_assets/xovain/logo.png">
+                <h3>You've never seen beauty like this before.</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 push-2 pad-mobile-20">
+            <p class="first">A behind-the-scenes look at Jane Pratt's newest media adventure.</p>
+            <p>When xoJane launched almost two years ago, it was truly one of the first websites where independent, passionate and opinionated women could go to find raw, honest and compelling stories from real, relatable women. Readers were drawn to the site instantly, spending an average of five minutes per visit engaging with the authors and each other in the comments. Even more impressive is how thoughtful the readers' reactions were to the personal articles they couldn't find anywhere else on the Web.</p>
+            <p>At the same time, these readers had no problem clicking from an article like <a href="#" title="It Happened to Me: I'm Married to a Virgin">It Happened to Me: I'm Married to a Virgin</a> to a beauty article telling them how one writer <a href="#" title="fixed her chicken skin and got awesome hair to boot">fixed her chicken skin and got awesome hair to boot</a>.</p>
+        </div>
+        <div class="grid-2 push-2 pad-mobile-20 hide-mobile">
+            <img src="_img/social.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 pad-mobile-20">
+            <p>In fact, many of those beauty articles were among the site's most popular. Readers' loved that they were getting beauty advice from women who'd actually tried the products and refused to say good things about something if it didn't do what it promised. Soon, the comment sections on beauty articles turned into a forum for the readers to give and get great makeup and skin care advice from each other. Beauty articles garnered hundreds of comments in just a few hours.</p>
+            <p>"The beauty vertical on xoJane blew up from the very beginning," Pratt says. "It was one of our biggest categories by far. So when we were looking at the best way to grow the brand next, we took a close look at just why it was so popular and what else was out there in terms of strict beauty sites. Turns out there was a real hole in the marketplace in terms of the type of beauty articles on xoJane and the type of beauty site we could do." Pratt also wanted to capitalize on xoJane's success as the fastest-growing women's lifestyle brand on the Web. With more than 1.5 million monthly readers and 6.2 million monthly page views, Pratt knew there was an opportunity there.</p>
+        </div>
+        <div class="pull-1 grid-3 push-2 pad-mobile-20 photo hide-mobile">
+            <img src="_assets/xovain/chipotle.png" />
+            <p class="caption">Chipotle’s little world come to life.</p>
+        </div>
+    </div>
+</section>
+
+<section class="content dark">
+    <div class="lovely-grid">
+        <div class="grid-15 pull-4 push-5">
+            <blockquote><i class="icon-caret-down"></i><strong>Enter: xoVain</strong> the newest extension of the "xo" brand and a beauty site like no other.</blockquote>
+        </div>
+        <div class="grid-10 pull-8 pad-mobile-20">
+            <p>Pratt says, "xoVain is not about professional experts. It's about real women sharing what they know and have learned about beauty by living it." Pratt explains that a woman who has been dealing with acne since the age of 13 is an expert in skin care because she's lived it. She's tried everything. She knows what works and what doesn't. She doesn't need a degree to make her opinion valid. Her opinion is valid because she's talking about her own experiences.</p>
+            <p>xoVain is similar to xoJane in that there is a cast of characters the readers can really get to know. Women of all ages (the target demographic is 18–49) can find someone on the site who has similar beauty issues or a similar look. And they can get to know about the editors' personal lives through the ways in which the editors describe their beauty routines, experiments, and tips and tricks. Pratt says, "Ideally, the readers will become more invested in the writers and the beauty advice they're giving because, for example, they'll know that a writer was out until 3 a.m. the night before and had to get up for a 9 a.m. job interview. When that writer shows how she got rid of her dark circles and made her eyes look awake, the readers will know it's real and they'll trust her. Real life experiences. Real advice."</p>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-12 pull-8 push-4 pad-mobile-20">
+            <img src="_assets/xovain/editors.png">
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="lovely-grid">
+        <div class="grid-10 pull-8 push-6 pad-mobile-20">
+            <p>The cast of characters is not yet complete, but Pratt is excited about the ones she's already hired. Marci Robin, beauty director, is "more of a nerd" Pratt says. "She knows all of the ingredients that go into every product. Compare that with Olga who, Pratt laughs, is "superficial with a capital S." ("She'll kill me for saying that," she says and then decides that actually Olga, a 29-year-old who writes about wrinkles that barely exist, won't mind at all.) Faz is "the girl who will try anything," and Annie is "the young wild child who just moved to New York City." Annie, Jane says, "is beautiful. Well everyone's beautiful," she caveats, "but Annie is model beautiful." To top it all off? "Oh, the twins! Pia and Runa. They're cute, bubbly, fun. Plus, they're twins!" Pratt exclaims. "Who doesn't love to see that?"</p>
+            <p>Of course, because it's a beauty site and beauty is all about products, application, how-tos and results, xoVain focuses heavily on the visuals. "The site is gorgeous," Pratt says, "and there is a lot more video. Our version of tutorials," she explains. "Quirky and very much in the situation. Like, I'm in the airport bathroom, and I've got to blow dry my hair." </p>
+            <p>What other types of articles can readers expect? Everything from "How to Shave Your Legs in the Sink and Explain it to Your Preschooler" to "Black Girls Can Wear Blue Eye Shadow, Okay?" Robin is personally eager to see the comments on her article about how people can be kind of stupid about natural beauty products. "Just because something's natural doesn't mean it's safer or better," she says. "You never hear about someone adding arsenic powder to their smoothie and exclaiming, "It's natural! Cheers!"</p>
+            <p>What is Pratt most excited about? "I can't wait to see these women whom I know are stars, but whom other people don't know yet. I can't wait to watch them blow up. That's always so much fun."</p>
+        </div>
+    </div>
+</section>
