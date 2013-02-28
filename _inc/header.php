ede5404f1c42c4c34338197d90f1ce62d7e4c157
Alex Schleifer <aschleifer@saymedia.com>
2013-02-28 15:56:29 -0800

    Cleaned up LESS, started on navigation.

--- _inc/header.php
+++ _inc/header.php
@@ -1,12 +1,28 @@
 <header>
+
     <div class="lovely-grid">
         <div class="grid-7">
+
             <ul class="tools">
-                <li><a href="#" title="View Contents"><i class="icon-reorder"></i></a></li>
+
+                <li>
+                    <a href="#" title="View Contents"><i class="icon-reorder"></i></a>
+
+                    <ul>
+                        <li>Cover</li>
+                        <li>Table of Contents</li>
+                        <li>Home</li>
+                        <li>Home</li>
+                    </ul>
+                </li>
+
+
             </ul>
+
             <h1 class="logo small remove-768 remove-960 remove-1200">
                 <a href="#" title="SAY Quarterly">SAY Quarterly</a>
             </h1>
+
         </div>
         <div class="grid-10">
             <h1 class="logo hide-mobile">
