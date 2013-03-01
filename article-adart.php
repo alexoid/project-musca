723841c1519ec6fc8e8cd48247ca46c4a06b6557
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-02 01:30:41 +0200

    Adart Images
    http://j76.me/NHMz > Articles > Adart > #1

--- article-adart.php
+++ article-adart.php
@@ -29,7 +29,7 @@
     </div>
 </section>
 
-<section class="content hide-mobile">
+<section class="content remove-mobile">
     <div class="lovely-grid">
         <div class="grid-22 push-2 pad-mobile-20">
             <div class="quote">
@@ -43,8 +43,11 @@
 
 <section class="content">
     <div class="lovely-grid">
-        <div class="grid-4 pull-2 push-1 pad-mobile-20 google hide-mobile">
+        <div class="grid-4 pull-2 push-1 pad-mobile-20 google remove-mobile">
             <p class="caption"><i class="icon-sort-up"></i> It’s Google flashing its technology through a gorgeous music video for <a href="#" title="Arcade Fire">Arcade Fire</a>.</p>
+        </div>
+        <div class="grid-4 pull-2 push-1 pad-mobile-20 google inline remove-768 remove-960 remove-1200">
+            <p class="caption"><i class="icon-caret-up"></i> It’s Google flashing its technology through a gorgeous music video for <a href="#" title="Arcade Fire">Arcade Fire</a>.</p>
         </div>
         <div class="grid-10 pull-1 push-6 pad-mobile-20">
             <p>Renaissance art is precious for awakening mankind’s creative soul, but who in their right mind defines Renaissance painting or sculpture as “advertising”? We say it’s “art,” right? Never mind that most masterpieces were bankrolled by deep-pocketed religious types with their own agendas.</p>
@@ -54,11 +57,14 @@
 
 <section class="content">
     <div class="lovely-grid">
+        <div class="grid-4 pull-2 push-1 pad-mobile-20 bmw inline remove-768 remove-960 remove-1200">
+            <p class="caption"><i class="icon-caret-left"></i> It’s BMW driving cars through a <a href="#" title="giant glass apple">giant glass apple</a>.</p>
+        </div>
         <div class="grid-10 pull-8 pad-mobile-20">
             <p>Fast forward to ad ads. You know, the ones from Ford, Gillette, McDonalds, etc. Advertising, as opposed to art, is all about the involuntary experience of companies disrupting our space and time. Ads disrupt entertainment (TV); ads disrupt local environments (billboards); ads even disrupt the dietary habits of fictional characters in movies sometimes.</p>
             <p>But good advertising really is art. And the people who craft it well are amazing artists. That ad design you see may have been crafted by the Michelangelo of Adobe Illustrator and the copy penned by the Shakespeare of copywriting. A musical jingle you hear could be from the Beethoven of commercial scoring. Creative minds abound in advertising. These people focus their time and energy to craft something expressive and beautiful. The ads we see today just happen to have been shaped by corporate influence (the church?) and feature a logo (the cross?). </p>
         </div>
-        <div class="pull-1 grid-3 push-2 pad-mobile-20 bmw hide-mobile">
+        <div class="pull-1 grid-3 push-2 pad-mobile-20 bmw remove-mobile">
             <p class="caption"><i class="icon-sort-up"></i> It’s BMW driving cars through a <a href="#" title="giant glass apple">giant glass apple</a>.</p>
         </div>
     </div>
@@ -67,8 +73,11 @@
 
 <section class="content">
     <div class="lovely-grid">
-        <div class="grid-3 pull-2 push-2 pad-mobile-20 toshiba hide-mobile">
+        <div class="grid-3 pull-2 push-2 pad-mobile-20 toshiba remove-mobile">
             <p class="caption"><i class="icon-sort-up"></i> It’s Toshiba sending the <a href="#" title="first chair to space">first chair to space</a> attached to a weather balloon.</p>
+        </div>
+        <div class="grid-4 pull-2 push-1 pad-mobile-20 toshiba inline remove-768 remove-960 remove-1200">
+            <p class="caption"><i class="icon-caret-left"></i> It’s Toshiba sending the <a href="#" title="first chair to space">first chair to space</a> attached to a weather balloon.</p>
         </div>
         <div class="grid-10 pull-1 push-6 pad-mobile-20">
             <p>The world we live in today is completely mediated. As a result, consumers are often deeply suspicious that everyone is trying to sell them something. What’s an advertiser to do? Well, it’s easy to not like ads. But it’s harder to not like good art. Any marketing department has a lot to learn from the modern-day Caravaggios and Berninis of the world, employed by ad agencies or not. The whole industry needs its own revolution in making its consumers feel inspired — not sold to.</p>
