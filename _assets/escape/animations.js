d03f7f8896cb8ecbcb248cc14fb1e7bfc72ce5e8
Kyle Derkacz <kderkacz@saymedia.com>
2013-03-01 07:13:36 -0800

    Smoothing out the profile picture animations on Escape.

--- _assets/escape/animations.js
+++ _assets/escape/animations.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     var controller = $.superscrollorama();
 
-    controller.addTween('#sarah-rotman', TweenMax.from( $('#sarah-rotman'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}), 0, -200);
-    controller.addTween('#jp-rangaswami', TweenMax.from( $('#jp-rangaswami'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}), 0, -100);
-    controller.addTween('#vivek-wadhwa', TweenMax.from( $('#vivek-wadhwa'), .25, {css:{left:'1000px'}, ease:Quad.easeInOut}), 0, -20);
-    controller.addTween('#david-sobotta', TweenMax.from( $('#david-sobotta'), .25, {css:{left:'1000px'}, ease:Quad.easeInOut}), 0, -100);
-    controller.addTween('#alan-cohen', TweenMax.from( $('#alan-cohen'), .25, {css:{left:'1000px'}, ease:Quad.easeInOut}), 0, -150);
+    controller.addTween('#sarah-rotman', TweenMax.from( $('#sarah-rotman'), .75, {css:{right:'1000px', rotation: -100}, ease:Quad.easeInOut}), 0, -200);
+    controller.addTween('#jp-rangaswami', TweenMax.from( $('#jp-rangaswami'), .75, {css:{right:'1000px', rotation: -540}, ease:Quad.easeInOut}), 0, 100);
+    controller.addTween('#vivek-wadhwa', TweenMax.from( $('#vivek-wadhwa'), .75, {css:{left:'1000px', rotation: 180}, ease:Quad.easeInOut}), 0, -20);
+    controller.addTween('#david-sobotta', TweenMax.from( $('#david-sobotta'), .75, {css:{left:'1000px', rotation: 120}, ease:Quad.easeInOut}), 0, -100);
+    controller.addTween('#alan-cohen', TweenMax.from( $('#alan-cohen'), .75, {css:{left:'1000px', rotation: 240}, ease:Quad.easeInOut}), 0, -150);
 });
