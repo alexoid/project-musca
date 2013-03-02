1ce503ee409cad45b26d8f2c5771f59b1030d82c
Alexoid <alexoid@gmail.com>
2013-03-01 19:10:33 -0800

    The site is fixed again (I broke it?) and now committing changes to navigation.

--- _inc/header.php
+++ _inc/header.php
@@ -1,46 +1,50 @@
-<header>
+<header id="navigation">
 
-    <div class="lovely-grid">
-        <div class="grid-7">
+    <div class="menu">
 
-            <ul class="tools">
+        <div class="main">
 
-                <li>
-                    <a href="#" title="View Contents"><i class="icon-reorder"></i></a>
+            <div class="logo">
+                <a href="#" title="Say Quarterly">Say Quarterly <i class="icon-reorder"></i></a>
+            </div>
 
-                    <ul>
-                        <li><a href="/">Coverpage</a></li>
-                        <li><a href="/article-adart.php">Great Advertising is Art</a></li>
-                        <li><a href="/article-canon.php">Behind The Lens</a></li>
-                        <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
-                        <li><a href="/article-london.php">Good Advertising, Strong Communities and Bob Dylan</a></li>
-                        <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
-                        <li><a href="/article-tempest.php">Meet Tempest</a></li>
-                        <li><a href="/article-xovain.php">Introducing xoVain</a></li>
-                        <li><a href="/feature-escape.php">The Great Escape</a></li>
-                        <li><a href="/feature-storytelling.php.php">The Art and Science of Great Brand Storytellin</a></li>
+            <div class="issue">
+                Issue V &middot; Spring 2013
+            </div>
+
+        </div>
 
 
-                    </ul>
-                </li>
+        <div class="expand">
 
-
+            <ul>
+                <li><a href="/">Cover</a></li>
+                <li><a href="/article-adart.php">Great Advertising is Art</a></li>
+                <li><a href="/article-london.php">Good Ads and Bob Dylan</a></li>
             </ul>
 
-            <h1 class="logo small remove-768 remove-960 remove-1200">
-                <a href="#" title="SAY Quarterly">SAY Quarterly</a>
-            </h1>
+            <ul>
+                <li>Features</li>
+                <li><a href="/feature-escape.php">The Great Escape</a></li>
+                <li><a href="/feature-storytelling.php">Art and Science of Brand Storytelling</a></li>
+                <li><a href="/feature-unusual.php">Unusual Suspects</a></li>
+            </ul>
+
+            <ul>
+                <li>Also</li>
+                <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
+                <li><a href="/article-tempest.php">Meet Tempest</a></li>
+                <li><a href="/article-xovain.php">Introducing xoVain</a></li>
+            </ul>
+
+            <ul>
+                <li>Case Studies</li>
+                <li><a href="/article-canon.php">Behind The Lens</a></li>
+                <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
+            </ul>
 
         </div>
-        <div class="grid-10">
-            <h1 class="logo hide-mobile">
-                <a href="#" title="Say Quarterly">Say Quarterly</a>
-            </h1>
-        </div>
-        <div class="grid-7 hide-mobile">
-            <p class="issue text-right">
-                Issue V &middot; Spring 2013
-            </p>
-        </div>
+
     </div>
+
 </header>
