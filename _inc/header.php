ab43716f5e495f15c2e97c2e5b752f69d40db745
Alexoid <alexoid@gmail.com>
2013-03-01 16:24:41 -0800

    Nav updates

--- _inc/header.php
+++ _inc/header.php
@@ -9,10 +9,18 @@
                     <a href="#" title="View Contents"><i class="icon-reorder"></i></a>
 
                     <ul>
-                        <li>Cover</li>
-                        <li>Table of Contents</li>
-                        <li>Home</li>
-                        <li>Home</li>
+                        <li><a href="/">Coverpage</a></li>
+                        <li><a href="/article-adart.php">Great Advertising is Art</a></li>
+                        <li><a href="/article-canon.php">Behind The Lens</a></li>
+                        <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
+                        <li><a href="/article-london.php">Good Advertising, Strong Communities and Bob Dylan</a></li>
+                        <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
+                        <li><a href="/article-tempest.php">Meet Tempest</a></li>
+                        <li><a href="/article-xovain.php">Introducing xoVain</a></li>
+                        <li><a href="/feature-escape.php">The Great Escape</a></li>
+                        <li><a href="/feature-storytelling.php.php">The Art and Science of Great Brand Storytellin</a></li>
+
+
                     </ul>
                 </li>
 
