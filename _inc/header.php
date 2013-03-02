f81a77d17f1fde3dadc0c9e1c90b7fb77976ad5d
Alexoid <alexoid@gmail.com>
2013-03-01 20:08:20 -0800

    Navigation system works. Needs to be pos: fixed and links (plus headings) need to be updated.

--- _inc/header.php
+++ _inc/header.php
@@ -4,8 +4,10 @@
 
         <div class="main">
 
+            <i class="icon-reorder"></i>
+
             <div class="logo">
-                <a href="#" title="Say Quarterly">Say Quarterly <i class="icon-reorder"></i></a>
+                <a href="#" title="Say Quarterly"><b>Say</b> Quarterly </a>
             </div>
 
             <div class="issue">
@@ -18,29 +20,33 @@
         <div class="expand">
 
             <ul>
+                <li class="header">Issue V</li>
                 <li><a href="/">Cover</a></li>
                 <li><a href="/article-adart.php">Great Advertising is Art</a></li>
-                <li><a href="/article-london.php">Good Ads and Bob Dylan</a></li>
+                <li><a href="/article-adart.php">Publisher's Letter</a></li>
+                <li><a href="/article-adart.php">Table of Contents</a></li>
             </ul>
 
             <ul>
-                <li>Features</li>
+                <li class="header">Features</li>
                 <li><a href="/feature-escape.php">The Great Escape</a></li>
                 <li><a href="/feature-storytelling.php">Art and Science of Brand Storytelling</a></li>
                 <li><a href="/feature-unusual.php">Unusual Suspects</a></li>
             </ul>
 
             <ul>
-                <li>Also</li>
+                <li class="header">Articles</li>
                 <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
                 <li><a href="/article-tempest.php">Meet Tempest</a></li>
                 <li><a href="/article-xovain.php">Introducing xoVain</a></li>
+                <li><a href="/article-london.php">Good Ads and Bob Dylan</a></li>
             </ul>
 
             <ul>
-                <li>Case Studies</li>
+                <li class="header">Specials</li>
                 <li><a href="/article-canon.php">Behind The Lens</a></li>
                 <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
+                <li><a href="/article-adart.php">Great Advertising is Art</a></li>
             </ul>
 
         </div>
