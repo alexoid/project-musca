685ef76e31866472cf69ab845f9f5567387beb21
Alexoid <alexoid@gmail.com>
2013-03-05 19:11:22 -0800

    Edited UNUSUAL SUSPECTS. Started work on new homepage.

---
+++ index-old.php
@@ -0,0 +1,37 @@
+<style type="text/css" media="screen">
+    @import url("_assets/homepage/styles.css");
+</style>
+
+<section class="hero">
+    <div class="wrapper">
+        <div class="lovely-grid">
+            <div class="grid-24">
+                <h2 class="remove-mobile remove-768">THE POV<br/> ISSUE</h2>
+                <h2 class="medium remove-mobile remove-960 remove-1200">THE POV<br/> ISSUE</h2>
+                <h2 class="small remove-768 remove-960 remove-1200">THE POV<br/> ISSUE</h2>
+            </div>
+            <div class="grid-14 pull-5 push-5 dan">
+                <img src="_assets/homepage/dan.png" />
+            </div>
+            <div class="grid-8 push-3 pull-1 pad-mobile-20">
+                <div class="left-story">
+                    <h3>Cover Story</h3>
+                    <h1><a href="feature-escape.php" title="The Great Escape">The Great Escape</a></h1>
+                    </div>
+            </div>
+            <div class="grid-8 pull-3 push-1 text-right pad-mobile-20">
+                <div class="right-story">
+                    <h3>Also</h3>
+                    <ul>
+                        <li><a href="feature-unusual.php" title="Unusual Suspects">Unusual Suspects</a></li>
+                        <li><a href="feature-storytelling.php" title="Art of Storytelling">Art of Storytelling</a></li>
+                        <li><a href="article-kate.php" title="Kate Upton">Kate Upton</a></li>
+                        <li><a href="/toc.php" title="And more" class="more">And more</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
