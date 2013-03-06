016af52143132950d3f5287955eba8776822f9f7
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-07 01:28:59 +0200

    Cover Scroll

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     @import url("_assets/cover/styles.css");
 </style>
 
-<div id="background"><div class="inner"></div></div>
+<div id="background"></div>
 
 <section id="cover">
 <?php include "../toc.php"; ?>
