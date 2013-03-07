b605a1cab5a3441b1d8e2fb423527db753aa1bae
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-08 00:35:03 +0200

    The Great Escape (All Editions)

--- feature-escape.php
+++ feature-escape.php
@@ -29,11 +29,25 @@
     </section>
 
     <section class="content">
-        <div class="lovely-grid map">
-            <div class="grid-10 pull-7 push-7 pad-mobile-20">
+        <div class="lovely-grid">
+            <div class="grid-4 pull-2 push-1 pad-mobile-20 remove-mobile">
+                <img src="_assets/escape/map.png" />
+            </div>
+            <div class="grid-10 push-7 pad-mobile-20">
+                <div class="show-mobile">
+                    <img src="_assets/escape/map.png" class="map-picture mobile" />
+                </div>
                 <p>People keep asking me about my vision for ReadWrite, and to be honest, it's pretty simple. I'm trying to make the kind of tech news site that I've always wanted to read but couldn't find. I want to create a site where a handful of really great "dream team" writers deliver smart, lively, entertaining and provocative articles about what's happening in the world of tech. I call it "tech news for people who think."</p>
                 <p>ReadWrite needs to be timely and topical and on top of the news, but our role is not to report everything that happens. Rather, it is to sift through the noise and find the events and issues that really matter, and then explain them in a fresh and interesting way.</p>
                 <p>We want to be relevant and influential. We want to drive the conversation not follow it. We want our readers to be deeply engaged in our site, to be not just consumers of content but also creators. We are trying to build a site that readers turn to as a kind of home on the Web, a place where they will return every day. ReadWrite aims to become an online community where everyone has a voice. If we're really successful, the site will be not just a publication but also a conversation.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="lovely-grid">
+
+            <div class="grid-10 pull-7 push-7 pad-mobile-20">
                 <p>Say Media talks a lot about the virtues of "point-of-view publishing." The great news for ReadWrite is that this is not much of a stretch from what we've always done. The original idea behind ReadWriteWeb, which was the site's name when it was founded nearly a decade ago, was that the Web enabled a new kind of journalism, one in which everyone could read and write. We've dropped the "Web" part of the name simply because the world has moved beyond the Web, but the notion of creating a two-way flow of ideas remains a core principle for us.</p>
             </div>
         </div>
@@ -41,20 +55,30 @@
 
     <section class="content">
         <div class="lovely-grid">
-            <div class="grid-5 pull-1 push-1 pad-mobile-20">
+            <div class="grid-5 pull-1 push-1 pad-mobile-20 remove-mobile">
                 <img src="_assets/escape/Sarah-Rotman-Epps-PR.png" id="sarah-rotman" class="profile-picture" />
                 <img src="_assets/escape/JP-rangaswami.png" id="jp-rangaswami" class="profile-picture" />
             </div>
             <div class="grid-10 pad-mobile-20">
                 <h3>So whose point of view are we publishing?</h3>
+                <div class="show-mobile">
+                    <img src="_assets/escape/Sarah-Rotman-Epps-PR.png" class="profile-picture mobile" />
+                    <img src="_assets/escape/JP-rangaswami.png" class="profile-picture mobile" />
+                </div>
                 <p>There's mine, of course, plus the opinions of our staff writers and a half dozen or so contributors who have a lot of domain expertise and strong personal brands. I'm encouraging our writers to pursue their passions and to write from the heart about topics that matter most to them. We've created a channel called <a href="http://readwrite.com/series/pause" title="ReadWrite Pause" target="blank">ReadWrite Pause</a>, led by a fantastic contributing writer, Forrester Research analyst <a href="http://readwrite.com/author/sarah-rotman-epps" title="Sarah Rotman Epps" target="blank">Sarah Rotman Epps</a>. Sarah uses this channel to explore a wide range of topics including digital addiction and the need to step away from the Internet rather than be tethered to the digital umbilical cord 24 hours a day.</p>
                 <p>We're trying to bring in lots of exciting and important voices to deliver an insider's view on how the tech industry operates. Recently we stirred up a huge debate by giving Internet entrepreneur Mark Cuban a platform to <a href="http://readwrite.com/2012/11/13/mark-cuban-facebooks-sponsored-posts-are-driving-away-brands" title="complain about the way Facebook is treating big brands" target="blank">complain about the way Facebook is treating big brands</a>. That story generated a lot of traffic for us, but what's more important is that the article got people thinking and talking. Soon after we ran a story by Bernard Meisler, an engineer at Say Media, who had noticed that <a href="http://readwrite.com/2012/12/11/why-are-dead-people-liking-stuff-on-facebook" title="dead people were continuing to like things on Facebook" target="blank">dead people were continuing to like things on Facebook</a> and wondered how this was possible. The story looked at the plague of fake "likes" on Facebook, which turns out to be a huge issue for Facebook's members and advertisers alike.</p>
                 <p>Some of our other new contributors include <a href="http://readwrite.com/author/vivek-wadhwa" title="Vivek Wadhwa" target="blank">Vivek Wadhwa</a>, an academic who was recently named one of Foreign Policy magazine's Top 100 global thinkers; <a href="http://readwrite.com/author/jp-rangaswami" title="J.P. Rangaswami" target="blank">J.P. Rangaswami</a>, the chief scientist at salesforce.com; David Sobotta, a former top executive at Apple, who writes compellingly about his alma mater; and Alan Cohen, a former top executive at Cisco and Nicira, who writes with wit and intelligence about issues facing large enterprises and the disruption of traditional IT.</p>
                 <h3>We're also inviting interesting people in the industry to contribute articles.</h3>
-                <p>We're tracking the best writers we know and synopsizing their work or, when possible, syndicating their articles. The vision is to turn ReadWrite into a lively online salon, a place where you come to find great articles by smart people.</p>
+                <div class="show-mobile">
+                    <a href="http://readwrite.com/author/vivek-wadhwa" title="Vivek Wadhwa" target="blank"><img src="_assets/escape/vivek-wadhwa.png" class="profile-picture mobile" /></a>
+                    <a href="http://readwrite.com/author/david-sobotta" title="David Sobatta" target="blank"><img src="_assets/escape/David-Sobotta.png" class="profile-picture mobile" /></a>
+                    <a href="http://readwrite.com/author/alan-s-cohen" title="Alan Cohen" target="blank"><img src="_assets/escape/Alan-Cohen.png" class="profile-picture mobile" /></a>
+                </div>
+                <p>We're tracking the best writers we know and synopsizing their work or, when possible syndicating their articles. The vision is to turn ReadWrite into a lively online salon, a place where you come to find great articles by smart people.</p>
                 <p>It's an exciting time to be working in the media business. Everything is being disrupted and reinvented. We're dreaming up new ways to tell stories, new ways for brands to connect with consumers. It's all moving at a breakneck pace, and I'm thrilled and excited to be part of this change.</p>
             </div>
-            <div class="grid-5 pull-1 push-1 pad-mobile-20">
+
+            <div class="grid-5 pull-1 push-1 pad-mobile-20 remove-mobile">
                 <a href="http://readwrite.com/author/vivek-wadhwa" title="Vivek Wadhwa" target="blank"><img src="_assets/escape/vivek-wadhwa.png" id="vivek-wadhwa" class="profile-picture" /></a>
                 <a href="http://readwrite.com/author/david-sobotta" title="David Sobatta" target="blank"><img src="_assets/escape/David-Sobotta.png" id="david-sobotta" class="profile-picture" /></a>
                 <a href="http://readwrite.com/author/alan-s-cohen" title="Alan Cohen" target="blank"><img src="_assets/escape/Alan-Cohen.png" id="alan-cohen" class="profile-picture" /></a>
@@ -69,7 +93,7 @@
                 <p>
                     <strong>Dan Lyons</strong> is editor in chief of ReadWrite. Previously, he was technology editor at Newsweek and spent a decade at Forbes covering technology. He was also the creator of The Secret Diary of Steve Jobs, a satirical blog written in the persona of Fake Steve Jobs.</p>
                    <p><a href="https://twitter.com/realdanlyons" class="twitter-follow-button" data-show-count="false">Follow @realdanlyons</a></p>
-                
+
             </div>
         </div>
     </section>
