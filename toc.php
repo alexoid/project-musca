805b355ca3535e6ca5749870113c90611c9b8d69
Rachel <wolan8r@gmail.com>
2013-03-07 09:27:35 -0800

    TOC text changes

--- toc.php
+++ toc.php
@@ -2,25 +2,25 @@
         <div>
             <ul>
                 <li class="title">In this issue</li>
+                
+                <li><a href="./">Publisher's Letter</a></li>
 
                 <li class="header">Features</li>
                 <li><a href="./feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
-                <li><a href="./feature-storytelling.php">Art and Science of Brand Storytelling</a> <span>By John Thomas</span></li>
+                <li><a href="./feature-storytelling.php">Art and Science of Brand Storytelling</a> <span>By Jon Thomas</span></li>
                 <li><a href="./feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
 
                 <li class="header">Perspectives</li>
                 <li><a href="./article-adart.php">Great Advertising is Art</a> <span>By Joe Sabia</span></li>
-                <li><a href="./article-london.php">Good Ads and Bob Dylan</a> <span>By Jasin Lydiate</span></li>
+                <li><a href="./article-london.php">Good Ads and Bob Dylan</a> <span>By Jason Lydiate</span></li>
                 <li><a href="./article-tempest.php">Meet Tempest</a> <span>By David Lerman</span></li>
-                <li><a href="./article-xovain.php">Introducing xoVain</a> <span>By Someone</span></li>
+                <li><a href="./article-xovain.php">Introducing xoVain</a></li>
 
                 <li class="header">Case Studies</li>
                 <li><a href="./article-savoring.php">Savoring Authenticity</a></li>
                 <li><a href="./article-canon.php">Behind The Lens</a></li>
                 <li><a href="./article-kate.php">Kate Upton Takes Zoo York</a></li>
 
-                <li class="header">Also</li>
-                <li><a href="./">Publisher's Letter</a></li>
 
             </ul>
         </div>
