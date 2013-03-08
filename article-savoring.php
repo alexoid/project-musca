f9d8400c39b675ffc818af979b3d77225728d7ae
Rachel <wolan8r@gmail.com>
2013-03-08 12:40:24 -0800

    Text

--- article-savoring.php
+++ article-savoring.php
@@ -70,9 +70,9 @@
 <section class="content">
     <div class="lovely-grid">
         <div class="grid-10 pull-8 push-6 pad-mobile-20">
-        <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves have a shared passion for innovation, craftsmanship, and attention to detail, and who understand that taste defines the way in which one lives. It also excels at smart storytelling and were able to tie in Amstel Light's brand message through authentic interview questions about creating a small business like, "What complexities surprised you?"</p>
-        <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to SAY Media's extensive network, effectively reaching Amstel Light's target audience in a way it appreciated, but didn't expect.</p>
-        <p>Unusual advertising that's authentic, subtle, and complex? We'll definitely drink to that.</p>
+        <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves have a shared passion for innovation, craftsmanship and attention to detail, and who understand that taste defines the way in which one lives. It also excels at smart storytelling and were able to tie in Amstel Light's brand message through authentic interview questions about creating a small business like, "What complexities surprised you?"</p>
+        <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to Say Media's extensive network, effectively reaching Amstel Light's target audience in a way it appreciated, but didn't expect.</p>
+        <p>Unusual advertising that's authentic, subtle and complex? We'll definitely drink to that.</p>
         </div>
     </div>
 </section>
