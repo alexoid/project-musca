3916e3efd0c541491938f1691f6b66045b1e7590
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-08 12:02:30 +0200

    Pub Letter Tweaks

--- pub-letter.php
+++ pub-letter.php
@@ -20,7 +20,7 @@
 
 <section class="content">
     <div class="lovely-grid">
-        <div class="grid-5 push-1 pad-mobile-20 masthead">
+        <div class="grid-5 push-1 pad-mobile-20 masthead remove-mobile">
             <ul>
                 <li>
                     <h4>PUBLISHER</h4>
@@ -75,19 +75,15 @@
         <div class="grid-10 push-8 pad-mobile-20">
             <p class="first">In the media and marketing worlds, we frequently throw around a few expressions about
                 content.</p>
-
             <p>You’ve all heard them: Be a part of the conversation. Think like a publisher. Or the one I
                 really dislike: We need a content strategy. </p>
-
             <p>What all of those are really saying is this: We need a point of view. If you can figure out your point of
                 view, you’ll be able to carve out a place in the world of digital media that will be the envy of your
                 competitors.</p>
-
             <p>The power of point of view is not unique to digital publishing, of course. We’ve seen it define and shape
                 successful magazines, newspapers and cable TV shows (hello, Fox News). But the fact that it’s now
                 defining the successful digital media properties signals that digital media is approaching its golden
                 age. </p>
-
             <p>That’s why we devoted this issue of Say Magazine to the power of what we call Point-of-View Publishing
                 and the pioneers who are leading the way. In our cover story (“The Great Escape,” page xx), Dan Lyons,
                 editor in chief of ReadWrite, who you also may know as an award-winning columnist and the creator of
@@ -96,7 +92,6 @@
                 pictured him on the cover of the issue in a prison jumpsuit for fun but also to signal that you should
                 expect the unexpected from Lyons’ vision for ReadWrite (and we mean that in the best way
                 imaginable).</p>
-
             <p>Also in this issue, you'll find interviews some of the Web’s most talented — and in some cases surprising
                 — storytellers (“Unusual Suspects,” page xx). These content creators are narrators of the everyday as
                 well as the remarkable who use images, words or social media to engross an audience that is more
@@ -106,16 +101,66 @@
                 have attracted motivated and sizable loyal audiences. We’ve also created a cool map that highlights some
                 of the most promising and interesting point-of-view publishers and storytellers on page xx (prepare to
                 be surprised). </p>
-
             <p>On page xx, Jon Thomas, editor in chief of Post Advertising, takes point of view a step further in “The
                 Art and Science of Great Brand Storytelling” and shows you how to unearth your own brand story. We also
                 asked Brian Clark, one of the foremost experts on creating great copy to write about how to create great
                 online content that builds your business (“The State of the Art in Content Marketing,” page xx). </p>
-
             <p>And there’s much, much more. Taken together, all these stories are a playbook for what we believe it
                 takes for advertisers, content creators and all of us to be successful, relevant and competitive in this
                 brave new world of digital media.</p>
+        </div>
 
+        <div class="grid-5 push-1 pad-mobile-20 masthead show-mobile">
+            <ul>
+                <li>
+                    <h4>PUBLISHER</h4>
+                    Matt Sanchez
+                </li>
+                <li>
+                    <h4>MANAGING EDITOR</h4>
+                    Wendy Taylor
+                </li>
+                <li>
+                    <h4>CONTRIBUTING EDITOR</h4>
+                    Erin Olsson
+                </li>
+                <li>
+                    <h4>CREATIVE DIRECTOR</h4>
+                    Alex Schleifer
+                </li>
+                <li>
+                    <h4>SENIOR DESIGNER</h4>
+                    Sarah Teubner
+                </li>
+                <li>
+                    <h4>DESIGN AND ENGINEERING</h4>
+                    Mira Kim<br/>
+                    Constantinos Demetriadis<br/>
+                    Kyle Derkacz
+                </li>
+                <li>
+                    <h4>PRODUCT MANAGER</h4>
+                    Rachel Wolan
+                </li>
+                <li>
+                    <h4>PHOTO EDITOR</h4>
+                    Madeleine Weiss
+                </li>
+                <li>
+                    <h4>CONTRIBUTING ILLUSTRATORS</h4>
+                    Jesse Hayes<br/>
+                    Jenny San Martin<br/>
+                    Evelina Sorokoviha
+                </li>
+                <li>
+                    <h4>COPY EDITOR</h4>
+                    Michelle D. Williams
+                </li>
+                <li>
+                    <h4>PROOFREADER</h4>
+                    Katie Funk
+                </li>
+            </ul>
         </div>
 </section>
 
