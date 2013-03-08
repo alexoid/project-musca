63794a5e8be42a81c26fbb42d5776e55af32f647
Rachel <wolan8r@gmail.com>
2013-03-08 11:44:03 -0800

    Text changes to TOC

--- toc.php
+++ toc.php
@@ -7,16 +7,16 @@
 
                 <li class="header">Point of View</li>
                 <li><a href="./feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
-                <li><a href="./feature-storytelling.php">Art and Science of Brand Storytelling</a> <span>By Jon Thomas</span></li>
+                <li><a href="./feature-storytelling.php">The Art and Science of Great Brand Storytelling</a> <span>By Jon Thomas</span></li>
                 <li><a href="./feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
 
                 <li class="header">Perspectives</li>
-                <li><a href="./article-adart.php">Great Advertising is Art</a> <span>By Joe Sabia</span></li>
+                <li><a href="./article-adart.php">Great Advertising Is Art</a> <span>By Joe Sabia</span></li>
                 <li><a href="./article-savoring.php">Savoring Authenticity</a> <span>With Amstel Light</span></li>
                 <li><a href="./article-canon.php">Behind the Lens</a> <span>With Canon</span></li>
                 <li><a href="./article-kate.php">Kate Upton</a> <span>Takes Zoo York</span></li>
-                <li><a href="./article-london.php">Good Ads and Bob Dylan</a> <span>By Jason Lydiate</span></li>
-                <li><a href="./article-tempest.php">Meet Tempest</a> <span>By David Lerman</span></li>
+                <li><a href="./article-london.php">Good Advertising, Strong Communities and Bob Dylan</a> <span>By Jason Lydiate</span></li>
+                <li><a href="./article-tempest.php">Meet Tempest, Our Next-Generation Media Platform</a> <span>By David Lerman</span></li>
                 <li><a href="./article-xovain.php">A New Kind of Beauty Site</a> <span>Introducing xoVain</span></li>
 
             </ul>
