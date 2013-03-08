a6e3c0c261961f32b25b116456bafc168d084df9
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-08 12:37:12 +0200

    Savoring Auth Tweaks

--- article-savoring.php
+++ article-savoring.php
@@ -24,9 +24,13 @@
         <div class="grid-10 pull-1 push-2 pad-mobile-20">
             <p class="first">Amstel Light celebrates American entrepreneurs in a captivating custom video campagin.</p>
 
-            <div class="grid-5 pull-2 inline remove-768 remove-960 remove-1200">
+            <div class="social-share show-mobile">
+                <?php include "../_inc/social.php"; ?>
+            </div>
+
+            <div class=" inline show-mobile">
                 <div class="photo">
-                    <img src="_assets/zoo-york/video.png" alt="">
+                    <img src="_assets/savoring-auth/video-sm.jpg" alt="">
                     <p class="caption">
                         See it in action
                         <span>0:15 sec</span>
@@ -37,7 +41,7 @@
             <p>In a time when most beer advertisers inundate their audience with big, bold ads that cater to the lowest common denominator, it takes courage for a brand to trust that the people they truly want reach are intelligent, discerning, and sophisticated, and to provide a marketing experience that's in line with those beliefs. However, that is exactly what Amstel Light did when they launched their new campaign entitled <a href="http://gearpatrol.com/tag/amstel-light/" title="Savor Complexity" target="blank">Savor Complexity</a>."</p>
             <p>In an effort to encourage consumers to explore the complexities and appreciate the intricacies of their own lives, Amstel Light partnered with Say Media and Gear Patrol to create a video series profiling local entrepreneurs who are foraging new paths in their cities in order to show the value of complexity.</p>
         </div>
-        <div class="social-share grid-2 push-2 pad-mobile-20 hide-mobile">
+        <div class="social-share grid-2 push-2 pad-mobile-20 remove-mobile">
             <?php include "../_inc/social.php"; ?>
         </div>
     </div>
@@ -47,7 +51,7 @@
     <div class="lovely-grid">
         <div class="grid-5 pull-2 remove-mobile">
             <div class="photo">
-                <img src="_assets/savoring-auth/video.png" alt="">
+                <img src="_assets/savoring-auth/video.jpg" alt="">
                 <p class="caption hide-mobile">
                     See it in action
                     <span>0:15 sec</span>
