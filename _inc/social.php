33431b1de081284b4d7bba2935f0837d8474b5a4
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-08 11:55:14 +0200

    Moved Twitter script into Layout, Fixes Twitter follow but. everywhere

--- _inc/social.php
+++ _inc/social.php
@@ -3,18 +3,5 @@
 <div class="share-fb">
     <fb:like send="false" layout="button_count" width="120" show_faces="false"></fb:like>
 </div>
-
-<div class="share-twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-via="saymedia"
-                              data-related="saymedia" data-hashtags="sayquarterly">Tweet</a>
-    <script>!function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (!d.getElementById(id)) {
-                js = d.createElement(s);
-                js.id = id;
-                js.src = "//platform.twitter.com/widgets.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }
-        }(document, "script", "twitter-wjs");</script>
-</div>
-
+<div class="share-twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-via="saymedia" data-related="saymedia" data-hashtags="sayquarterly">Tweet</a></div>
 <div class="g-plusone" data-size="tall" data-annotation="none"></div>
