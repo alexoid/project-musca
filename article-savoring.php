01c7e08093588f388e5fafd8ffa69f0ac361fdd5
Rachel <wolan8r@gmail.com>
2013-03-08 12:36:41 -0800

    Text - Adart, Savoring

--- article-savoring.php
+++ article-savoring.php
@@ -38,7 +38,7 @@
                 </div>
             </div>
 
-            <p>In a time when most beer advertisers inundate their audience with big, bold ads that cater to the lowest common denominator, it takes courage for a brand to trust that the people they truly want reach are intelligent, discerning, and sophisticated, and to provide a marketing experience that's in line with those beliefs. However, that is exactly what Amstel Light did when they launched their new campaign entitled <a href="http://gearpatrol.com/tag/amstel-light/" title="Savor Complexity" target="blank">Savor Complexity</a>."</p>
+            <p>In a time when most beer advertisers inundate their audience with big, bold ads that cater to the lowest common denominator, it takes courage for a company to trust that the people it truly wants to reach are intelligent, discerning and sophisticated, and to provide a marketing experience that's in line with those beliefs. However, that is exactly what Amstel Light did when it launched their new campaign entitled <a href="http://gearpatrol.com/tag/amstel-light/" title="Savor Complexity" target="blank">Savor Complexity</a>."</p>
             <p>In an effort to encourage consumers to explore the complexities and appreciate the intricacies of their own lives, Amstel Light partnered with Say Media and Gear Patrol to create a video series profiling local entrepreneurs who are foraging new paths in their cities in order to show the value of complexity.</p>
         </div>
         <div class="social-share grid-2 push-2 pad-mobile-20 remove-mobile">
@@ -70,8 +70,8 @@
 <section class="content">
     <div class="lovely-grid">
         <div class="grid-10 pull-8 push-6 pad-mobile-20">
-        <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves have a shared passion for innovation, craftsmanship, and attention to detail, and who understand that taste defines the way in which one lives. They also excel at smart storytelling and were able to tie in Amstel Light's brand message through authentic interview questions about creating a small business like, "What complexities surprised you?"</p>
-        <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to SAY Media's extensive network, effectively reaching Amstel Light's target audience in a way they appreciated, but didn't expect.</p>
+        <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves have a shared passion for innovation, craftsmanship, and attention to detail, and who understand that taste defines the way in which one lives. It also excels at smart storytelling and were able to tie in Amstel Light's brand message through authentic interview questions about creating a small business like, "What complexities surprised you?"</p>
+        <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to SAY Media's extensive network, effectively reaching Amstel Light's target audience in a way it appreciated, but didn't expect.</p>
         <p>Unusual advertising that's authentic, subtle, and complex? We'll definitely drink to that.</p>
         </div>
     </div>
