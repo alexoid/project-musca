635c72faba0b72974d47a7c54c7e297937739a86
Alexoid <alexoid@gmail.com>
2013-03-07 18:58:50 -0800

    Added JS navigation for next/prev.

--- _inc/header.php
+++ _inc/header.php
@@ -1,3 +1,46 @@
+<script>
+var pageArr = ['pub-letter.php','index.php','article-adart.php','article-canon.php','article-kate.php','article-london.php','article-savoring.php','article-tempest.php','article-xovain.php','feature-escape.php','feature-storytelling.php','feature-unusual.php'];
+
+function getFileName() {
+    var url = document.location.href;
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+    url = url.substring(url.lastIndexOf("/") + 1, url.length);
+    return url;
+}
+
+function nextPage(){
+    var name = getFileName();
+    var next;
+    for(var c=0;c<pageArr.length;c++){
+        if( pageArr[c] == name ){
+            if(c==pageArr.length-1){
+                next = 0;
+            }else{
+                next = c+1;
+            }
+            location.href = pageArr[next];
+            return;
+        }
+    }
+}
+
+function prevPage(){
+    var name = getFileName();
+    var next;
+    for(var c=0;c<pageArr.length;c++){
+        if( pageArr[c] == name ){
+            if(c==0){
+                next = pageArr.length-1;
+            }else{
+                next = c-1;
+            }
+            location.href = pageArr[next];
+            return;
+        }
+    }
+}
+</script>
 <header id="navigation">
 
     <div class="menu">
@@ -5,8 +48,9 @@
 
             <div class="nav">
                 <a href="./"><i class="icon-home"></i></a>
-                <a href="./"><i class="icon-arrow-left"></i></a>
-                <a href="./"><i class="icon-arrow-right"></i></a>
+                <a href="./pub-letter.php"><i class="icon-info-sign"></i></a>
+                <a href="#" onclick="nextPage()"><i class="icon-arrow-left"></i></a>
+                <a href="#" onclick="prevPage()"><i class="icon-arrow-right"></i></a>
                 <?php /* <a href="./toc.php"><i class="icon-reorder"></i></a> */ ?>
             </div>
 
