5937d85708f40f888192b7adbf1f707d6a97cb13
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-10 21:15:36 +0200

    Savoring Auth Icon Play

--- article-savoring.php
+++ article-savoring.php
@@ -53,7 +53,7 @@
     <div class="lovely-grid">
         <div class="grid-5 pull-2 remove-mobile">
             <div class="photo">
-                <a href="https://vimeo.com/61373118" title="See it in action" class="fresco"><img src="_assets/savoring-auth/video.jpg" alt=""></a>
+                <a href="https://vimeo.com/61373118" title="See it in action" class="fresco darken"><img src="_assets/savoring-auth/video-sm.jpg" alt=""></a>
                 <p class="caption hide-mobile">
                     See it in action
                     <span>0:15 sec</span>
