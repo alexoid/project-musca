2fbcc535fa39a4ddf0bdf20867540f6f862904ab
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-11 11:29:59 +0200

    Savoting Auth Stats & Caption

--- article-savoring.php
+++ article-savoring.php
@@ -60,11 +60,21 @@
                 </p>
             </div>
         </div>
+        <div class="remove-mobile">
         <div class="grid-14 pull-1 push-2 pad-mobile-20">
             <div class="photo">
                 <img src="_assets/savoring-auth/photo-01.png" alt="">
                 <p class="caption right block">The ad drove three and half times the average click-through rate</p>
             </div>
+        </div>
+        </div>
+        <div class="show-mobile">
+        <div class="grid-14 pull-1 push-2 pad-mobile-20">
+            <div class="photo">
+                <img src="_assets/savoring-auth/photo-01.png" alt="">
+                <p class="caption mobile">The ad drove three and half times the average click-through rate</p>
+            </div>
+        </div>
         </div>
     </div>
 </section>
@@ -73,16 +83,19 @@
     <div class="lovely-grid">
         <div class="grid-10 pull-8 push-6 pad-mobile-20">
             <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves have a shared passion for innovation, craftsmanship and attention to detail, and who understand that taste defines the way in which one lives. It also excels at smart storytelling and were able to tie in Amstel Light's brand message through authentic interview questions about creating a small business like, "What complexities surprised you?"</p>
+            <div class="photo left show-mobile">
+                <img src="_assets/savoring-auth/stats.jpg" />
+            </div>
             <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to Say Media's extensive network, effectively reaching Amstel Light's target audience in a way it appreciated, but didn't expect.</p>
             <p>Unusual advertising that's authentic, subtle and complex? We'll definitely drink to that.</p>
         </div>
 
-        <div class="show-mobile">
+        <!-- <div class="show-mobile">
             <div class="grid-10 pull-8 push-6 pad-mobile-20">
                 <div class="photo">
                     <img src="_assets/savoring-auth/stats.jpg" />
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
