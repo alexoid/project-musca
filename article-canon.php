5f5c0f81f5d0c3d16c613987aecfbb43d9a13060
Constantinos Demetriadis <cdemetriadis@gmail.com>
2013-03-11 11:34:42 +0200

    Canon Stats

--- article-canon.php
+++ article-canon.php
@@ -65,6 +65,9 @@
                 </div>
 
                 <p>Gear Patrol was the perfect site to help Canon’s campaign come into focus. A significant portion of the site’s readers are both novice and experienced photographers who trust the Gear Patrol editors for tips and advice on what to buy and why. Gear Patrol captured readers’ attention by providing them with knowledge about DSLR cameras and lenses as well as a series on how to most effectively shoot using Canon DSLRs in different situations, including urban locations and at night.</p>
+                <div class="photo left show-mobile">
+                    <img src="_assets/canon/stats.jpg" />
+                </div>
                 <p>The posts lived on Gear Patrol’s site alongside high-impact, interactive ad units targeted to Say Media’s extensive network. As a result, photographers of all types were able to learn more about Canon’s DSLR offerings while also learning more about how to be a better photographer. Ultimately, the entire experience felt less like an ad and more like valuable content. An engaging photo gallery created by the editors increased time spent on the site relative to the consumer electronics benchmark.</p>
                 <p>A successful ad that benefits both the client and the consumer? Now that’s a picture worth capturing.</p>
             </div>
@@ -73,13 +76,13 @@
                 <img src="_assets/canon/eric.jpg">
             </div>
 
-            <div class="show-mobile">
+            <!-- <div class="show-mobile">
                 <div class="grid-14 pad-mobile-20">
                     <div class="photo">
                         <img src="_assets/canon/stats.jpg" />
                     </div>
                 </div>
-            </div>
+            </div> -->
 
         </div>
     </div>
