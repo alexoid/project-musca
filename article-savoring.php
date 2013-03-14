80118671751e03f43413a0f9a89398c73f8593cc
Michal Hotovec <michal.hotovec@gmail.com>
2013-03-14 16:06:26 +0100

    grid implementing Savoring size D

---
+++ article-savoring.php
@@ -0,0 +1,116 @@
+<section class="savoring-overide">
+
+
+    <!--hero begining    -->
+
+    <section id="hero">
+        <div class="diags">
+            <div class="wrapper">
+                <h3>Brand Case Study</h3>
+
+                <h2>Savoring<br/> Authenticity</h2>
+
+                <h3>Gear Patrol X Amstel Light</h3>
+            </div>
+        </div>
+    </section>
+
+    <!--    end of hero    -->
+
+
+    <!-- ---------------------------------------------------- -->
+    <!-- ---------------------------------------------------- -->
+
+
+    <section class="content">
+        <div class="grid">
+            <div class="outbox-left">
+                <div class="photo remove-mobile">
+                    <img src="_assets/savoring-auth/stats.jpg"/>
+                </div>
+
+
+            </div>
+
+            <?php include "../inc/social.php"; ?>
+
+            <div class="content-rail">
+                <p class="first">Amstel Light celebrates American entrepreneurs in a captivating custom video
+                    campaign.</p>
+
+                <p>In a time when most beer advertisers inundate their audience with big, bold ads that cater to the
+                    lowest common denominator, it takes courage for a company to trust that the people it truly wants to
+                    reach are intelligent, discerning and sophisticated, and to provide a marketing experience that's in
+                    line with those beliefs. However, that is exactly what Amstel Light did when it launched their new
+                    campaign entitled "<a href="http://gearpatrol.com/tag/amstel-light/" title="Savor Complexity"
+                                          target="blank">Savor Complexity</a>."</p>
+
+                <p>In an effort to encourage consumers to explore the complexities and appreciate the intricacies of
+                    their own lives, Amstel Light partnered with Say Media and Gear Patrol to create a video series
+                    profiling local entrepreneurs who are forging new paths in their cities in order to show the value
+                    of complexity.</p>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="grid">
+
+            <div class="outbox-left">
+                <div class="photo">
+                    <a href="https://vimeo.com/61373118" title="See it in action" class="fresco darken"><img
+                            src="_assets/savoring-auth/video-sm.jpg" alt=""></a>
+
+                    <p class="caption hide-mobile">
+                        See it in action
+                        <span>0:15 sec</span>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="remove-mobile">
+                <div class="content-rail-wide">
+                    <div class="photo">
+                        <img src="_assets/savoring-auth/photo-01.png" alt="">
+
+                        <p class="caption right block">The ad drove three and half times the average click-through
+                            rate</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="grid">
+            <div class="content-rail">
+                <p>Gear Patrol was the perfect fit for this campaign as both their readers and the creators themselves
+                    have a shared passion for innovation, craftsmanship and attention to detail, and who understand that
+                    taste defines the way in which one lives. It also excels at smart storytelling and were able to tie
+                    in Amstel Light's brand message through authentic interview questions about creating a small
+                    business like, "What complexities surprised you?"</p>
+
+
+                <p>The videos lived on Gear Patrol's site as well as in online and mobile ad units targeted to Say
+                    Media's extensive network, effectively reaching Amstel Light's target audience in a way it
+                    appreciated, but didn't expect.</p>
+
+                <p>Unusual advertising that's authentic, subtle and complex? We'll definitely drink to that.</p>
+            </div>
+
+            <!-- <div class="show-mobile">
+                <div class="grid-10 pull-8 push-6 pad-mobile-20">
+                    <div class="photo">
+                        <img src="_assets/savoring-auth/stats.jpg" />
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </section>
+
+</section>
