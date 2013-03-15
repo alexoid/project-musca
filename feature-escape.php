e78a6a229e82e8dec03ad351b9ecacfdef02e47b
Michal Hotovec <michal.hotovec@gmail.com>
2013-03-15 14:53:14 +0100

    removed backups, added feature escape template

---
+++ feature-escape.php
@@ -0,0 +1,25 @@
+
+
+
+
+    <!--hero begining    -->
+
+    <section id="hero">
+        <!-- hero subgrid -->
+        <section class="grid">
+
+            <div>hero</div>
+
+        </section>
+        <!-- hero subgird end-->
+    </section>
+
+    <!--    end of hero    -->
+
+
+    <!-- ---------------------------------------------------- -->
+    <!-- ---------------------------------------------------- -->
+
+
+
+
