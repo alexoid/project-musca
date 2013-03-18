84c5873b33785f5a71ffd96a1a5f6118c863f378
Michal Hotovec <michal.hotovec@gmail.com>
2013-03-18 10:10:16 +0100

    cleanup, filenames cleanup, file sorting, cove (home) page added to grid and overides

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<style type="text/css" media="screen">
-    @import url("css/cover.css");
-</style>
-
 <section class="home">
    <section id="theme"></section>
 
@@ -12,23 +8,23 @@
 
                 <li class="header">Features</li>
 
-                <li><a href="/_backup/feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
-                <li><a href="/_backup/feature-storytelling.php">Art and Science of Brand Storytelling</a>
+                <li><a href="/feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
+                <li><a href="/feature-storytelling.php">Art and Science of Brand Storytelling</a>
                     <span>By John Thomas</span></li>
-                <li><a href="/_backup/feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
+                <li><a href="/feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
 
                 <li class="header">Perspectives</li>
 
-                <li><a href="/_backup/article-adart.php">Great Advertising is Art</a> <span>By Joe Sabia</span></li>
-                <li><a href="/_backup/article-london.php">Good Ads and Bob Dylan</a> <span>By Jasin Lydiate</span></li>
-                <li><a href="/_backup/article-tempest.php">Meet Tempest</a> <span>By David Lerman</span></li>
-                <li><a href="/_backup/article-xovain.php">Introducing xoVain</a> <span>By Someone</span></li>
+                <li><a href="/article-adart.php">Great Advertising is Art</a> <span>By Joe Sabia</span></li>
+                <li><a href="/article-london.php">Good Ads and Bob Dylan</a> <span>By Jasin Lydiate</span></li>
+                <li><a href="/article-tempest.php">Meet Tempest</a> <span>By David Lerman</span></li>
+                <li><a href="/article-xovain.php">Introducing xoVain</a> <span>By Someone</span></li>
 
                 <li class="header">Case Studies</li>
 
-                <li><a href="/_backup/article-savoring.php">Savoring Authenticity</a></li>
-                <li><a href="/_backup/article-canon.php">Behind The Lens</a></li>
-                <li><a href="/_backup/article-kate.php">Kate Upton Takes Zoo York</a></li>
+                <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
+                <li><a href="/article-canon.php">Behind The Lens</a></li>
+                <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
 
                 <li class="header">Also</li>
 
