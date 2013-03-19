b249695254665769347f4c64eaef91c79d30a4bd
Michal Hotovec <michal.hotovec@gmail.com>
2013-03-19 11:41:23 +0100

    fixed cover - new image added pub-letter

--- index.php
+++ index.php
@@ -1,35 +1,31 @@
 <section class="home">
-   <section id="theme"></section>
+
+    <section id="theme"></section>
 
     <aside id="right-rail">
         <section class="toc">
-            <ul>
-                <li class="title">In this issue</li>
+            <div>
+                <ul>
+                    <li class="title">Issue VI</li>
 
-                <li class="header">Features</li>
+                    <li><a href="./pub-letter.php">Publisher's Letter</a></li>
 
-                <li><a href="/feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
-                <li><a href="/feature-storytelling.php">Art and Science of Brand Storytelling</a>
-                    <span>By John Thomas</span></li>
-                <li><a href="/feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
+                    <li class="header">Point of View</li>
+                    <li><a href="./feature-storytelling.php">The Art and Science of Great Brand Storytelling</a> <span>By Jon Thomas</span></li>
+                    <li><a href="./feature-unusual.php">Unusual Suspects</a> <span>By Lauren Streib</span></li>
+                    <li><a href="./feature-escape.php">The Great Escape</a> <span>By Dan Lyons</span></li>
 
-                <li class="header">Perspectives</li>
-
-                <li><a href="/article-adart.php">Great Advertising is Art</a> <span>By Joe Sabia</span></li>
-                <li><a href="/article-london.php">Good Ads and Bob Dylan</a> <span>By Jasin Lydiate</span></li>
-                <li><a href="/article-tempest.php">Meet Tempest</a> <span>By David Lerman</span></li>
-                <li><a href="/article-xovain.php">Introducing xoVain</a> <span>By Someone</span></li>
-
-                <li class="header">Case Studies</li>
-
-                <li><a href="/article-savoring.php">Savoring Authenticity</a></li>
-                <li><a href="/article-canon.php">Behind The Lens</a></li>
-                <li><a href="/article-kate.php">Kate Upton Takes Zoo York</a></li>
-
-                <li class="header">Also</li>
-
-                <li><a href="/">Publisher's Letter</a></li>
-            </ul>
+                    <li class="header">Perspectives</li>
+                    <li><a href="./article-adart.php">Great Advertising Is Art</a> <span>By Joe Sabia</span></li>
+                    <li><a href="./article-savoring.php">Savoring Authenticity</a> <span>With Amstel Light</span></li>
+                    <li><a href="./article-canon.php">Behind the Lens</a> <span>With Canon</span></li>
+                    <li><a href="./article-kate.php">Kate Upton</a> <span>Takes Zoo York</span></li>
+                    <li><a href="./article-london.php">Good Advertising, Strong Communities and Bob Dylan</a> <span>By Jason Lydiate</span></li>
+                    <li><a href="./article-tempest.php">Meet Tempest, Our Next-Generation Media Platform</a> <span>By David Lerman</span></li>
+                    <li><a href="./article-xovain.php">A New Kind of Beauty Site</a> <span>Introducing xoVain</span></li>
+                    <li><a href="./sceneheard.php">Scene and Heard</a> <span>At Say Media</span></li>
+                </ul>
+            </div>
         </section>
     </aside>
 </section>
---
+++ pub-letter.php
@@ -0,0 +1,254 @@
+<section id="hero">
+    <div class="grid">
+
+        <div class="wrapper">
+            <h3>Publisher's Letter</h3>
+
+            <h2 class="hide-mobile">The Power<br/> of Point of View</h2>
+        </div>
+
+    </div>
+</section>
+
+<section class="content">
+    <div class="grid">
+
+        <div class="outbox-left masthead">
+            <ul>
+                <li>
+                    <h3>Get in touch</h3><br/>
+                    Say Media<br/>
+                    245 5th Ave, 5th Floor<br/>
+                    New York, NY 10016<br/>
+                <li>
+                    <a href="mailto:magazine@saymedia.com">E-mail</a> ·
+                    <a href="http://saymedia.com">Website</a> ·
+                    <a href="http://facebook.com/saymediainc">Facebook</a>
+                </li>
+                <li>To inquire about becoming a publishing partner, contact Sarah Stringfellow at 415.738.6590</li>
+                <li>
+                    <a href="https://twitter.com/saymedia" class="twitter-follow-button" data-show-count="false">Follow
+                        @saymedia</a>
+                    <script>!function (d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "//platform.twitter.com/widgets.js";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, "script", "twitter-wjs");</script>
+                </li>
+            </ul>
+
+            <ul>
+                <li><h3>Advertising Sales</h3></li>
+                <li><h4>East Coast, New York</h4>
+
+                    Tim Koorbusch<br/>
+                    Vice President, U.S. Sales<br/>
+                    312.332.7500<br/><br/>
+
+                    Doug Barrett<br/>
+                    Vice President, Eastern Sales<br/>
+                    917.256.2760
+                </li>
+                <li>
+                    <h4>Midwest, Chicago</h4>
+                    Tim Carroll<br/>
+                    Vice President<br/>
+                    312.543.9023
+
+                </li>
+                <li>
+                    <h4>West Coast</h4>
+                    Leah Corselli<br/>
+                    West Coast Director<br/>
+                    310.709.8469
+                </li>
+                <li>
+                    <h4>Canada</h4>
+                    Paul Cassar<br/>
+                    Vice President, Canada<br/>
+                    514.370.2868
+                <li>
+                    <h4>United Kingdom</h4>
+                    Giles Ivey<br/>
+                    Managing Director<br/>
+                    +44 (0) 7930.393.485
+                </li>
+                <li>
+                    <h4>Australia</h4>
+                    Duncan Arthur<br/>
+                    Country Manager<br/>
+                    +61.433.394.892 Schleifer
+                </li>
+            </ul>
+
+            <ul>
+                <li><h3>Say Media</h3></li>
+                <li>Say Media is headquartered in New York and San Francisco with offices around
+                    the U.S. as well as Canada, the U.K. and Australia.
+                </li>
+                <li>
+                    <h4>CEO and co-founder</h4>
+                    Matt Sanchez
+                </li>
+                <li>
+                    <h4>President</h4>
+                    Kim Kelleher
+                </li>
+                <li>
+                    <h4>COO</h4>
+                    Sam Parker
+                </li>
+
+            </ul>
+
+            <ul>
+                <li><h3>Say Quarterly</h3></li>
+                <li>
+                    <h4>Publisher</h4>
+                    Matt Sanchez
+                </li>
+                <li>
+                    <h4>Managing Editor</h4>
+                    Wendy Taylor
+                </li>
+                <li>
+                    <h4>Contributing Editor</h4>
+                    Erin Olsson
+                </li>
+                <li>
+                    <h4>Creative Director</h4>
+                    Alex Schleifer
+                </li>
+                <li>
+                    <h4>Senior Designer</h4>
+                    Sarah Teubner
+                </li>
+                <li>
+                    <h4>Design and Engineering</h4>
+                    Mira Kim<br/>
+                    Constantinos Demetriadis<br/>
+                    Kyle Derkacz
+                </li>
+                <li>
+                    <h4>Product Manager</h4>
+                    Rachel Wolan
+                </li>
+                <li>
+                    <h4>Cover Photography</h4>
+                    Eliot Weisberg
+                </li>
+                <li>
+                    <h4>Photo Editor</h4>
+                    Madeleine Weiss
+                </li>
+                <li>
+                    <h4>Contributing Illustrators</h4>
+                    Jesse Hayes<br/>
+                    Jenny San Martin<br/>
+                    Evelina Sorokoviha
+                </li>
+                <li>
+                    <h4>Copy Editor</h4>
+                    Michelle D. Williams
+                </li>
+                <li>
+                    <h4>Proofreader</h4>
+                    Katie Funk
+                </li>
+            </ul>
+
+        </div>
+
+
+        <div class="content-rail">
+            <p class="first">If you're a frequent reader of our content, you'll know that each quarter we cover an
+                important theme in media and marketing. <strong>This time it's different.</strong></p>
+
+            <p>This issue of Say Quarterly (formerly Say Magazine) is for everyone who needs to connect with people,
+                whether at the office, on a website, as an editor or as a marketer. There is just too much media today —
+                too much competing for our attention. Worse, too much of it is bland, uninteresting or uninspiring — and
+                too easy to tune out. If you want to be noticed by your readers, customers or peers, you have to take a
+                stand (for better or worse). <strong>You need a point of view.</strong></p>
+
+            <p>There is no fence-sitting when it comes to Mike Bloomberg or Chris Christie. They are polarizing, but
+                people listen, and more important, people join the conversation when these two men take a position on an
+                issue. People rally passionately behind causes they identify with that are clearly articulated. The same
+                is true of the loyal followings that have propelled great magazines, TV shows and digital media brands.
+                FOX News, Girls and Jon Stewart have all built passionate audiences by unapologetically tackling issues
+                from their unique perspectives.</p>
+
+            <p><b>Simply put, point of view is brand.</b> And it's more important than topic. The people you enjoy
+                spending time with usually share your point of view. And when you share a point of view with someone,
+                you can talk about anything. That's true whether you create content, consume it or advertise with it.
+            </p>
+
+            <p>The fact that it's now defining the successful digital media properties signals that digital media is
+                approaching its golden age. What differentiates online media properties is not a domain name, an SEO
+                strategy or technology pyrotechnics. Ultimately, what attracts and keeps an audience engaged over time
+                is great storytelling and a clear and compelling point of view.</p>
+
+            <p>That's why we've devoted this issue of Say Quarterly to the power of what we call Point of View
+                Publishing — and the pioneers who are leading the way. In <a href="feature-escape.php"
+                                                                             title="The Great Escape">The Great
+                    Escape</a>, Dan Lyons, editor in chief of ReadWrite, award-winning columnist and the creator of Fake
+                Steve Jobs, talks about how he's harnessing storytelling and point of view as the secret to covering the
+                tech stories that really matter — and engaging readers in a two-way conversation. We've pictured him in
+                a prison jumpsuit for fun but also to signal that you should expect the unexpected from Dan's vision for
+                <a href="http://readwrite.com" title="ReadWrite" target="blank">ReadWrite</a> (and we mean that in the
+                best way imaginable).</p>
+
+            <p>Also in this issue of Say Quarterly, Lauren Streib of The Daily Beast interviews some of the Web's most
+                talented, and in some cases surprising, storytellers (<a href="feature-unusual.php"
+                                                                         title="The Unusual Suspects">Unusual
+                    Suspects</a>). These content creators are narrators of the everyday and the remarkable who use
+                images, words or social media to engross an audience that is more plugged-in than ever before. Media
+                entrepreneurs such as Kelly Framel from <a href="http://www.theglamourai.com/" title="The Glamourai"
+                                                           target="blank">The Glamourai</a>, Jamie Beck of <a
+                    href="http://annstreetstudio.com/" title="Ann Street Studio" target="blank">Ann Street Studio</a>,
+                Pippa Lord of <a href="http://sousstyle.com/" title="Sous Style" target="blank">Sous Style</a> and Emily
+                Schuman of <a href="http://cupcakesandcashmere.com/" title="Cupcakes and Cashmere" target="blank">Cupcakes
+                    and Cashmere</a> are strong, individual personalities who have cultivated significant influence, are
+                driving the e-commerce revolution, and have attracted motivated and sizable loyal audiences.</p>
+
+            <p>Also in this first edition, Jon Thomas, editor in chief of Post Advertising takes point of view a step
+                further in <a href="feature-storytelling.php" title="The Art and Science of Great Brand Storytelling">The
+                    Art and Science of Great Brand Storytelling</a> and shows you how to unearth your own brand story.
+                Say Media's chief technology officer and co-founder David Lerman previews the next-generation media
+                platform we're working on that will dramatically change the way media is created and consumed and that
+                is part of our vision for the media company of the future (<a href="article-tempest.php"
+                                                                              title="Meet Tempest">Meet Tempest, Our
+                    Next-Generation Media Platform</a>). And there's much, much more. Together, all these stories are a
+                playbook for what we believe it takes for great content, from editors and advertisers alike, to be
+                successful, relevant and competitive in this brave new world of digital media.</p>
+
+            <p>We hope you find this issue of Say Quarterly enlightening, entertaining — and above all useful. We look
+                forward to having a conversation with you about all of it.</p>
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+</section>
+
+<section class="content bio">
+    <div class="grid">
+        <div class="content-rail">
+            <img src="_assets/pub-letter/bio-matt.jpg"/>
+
+            <p><strong>Matt Sanchez</strong> is co-founder and CEO of Say Media.</p>
+
+            <p><a href="https://twitter.com/msanchez" class="twitter-follow-button" data-show-count="false">Follow
+                    @msanchez</a></p>
+        </div>
+    </div>
+</section>
