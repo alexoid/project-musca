ecaacb44ce63309b41e065f37eea4b57040512c6
Michal Hotovec <michal.hotovec@gmail.com>
2013-03-20 15:08:05 +0100

    mobile responsive versions (kate, adart, publetter, unusual); testing simple JQUERY switcher, for changing page structure and moving elements to different place in DOM based on document.width (1st test, article-adart.php)

--- article-adart.php
+++ article-adart.php
@@ -1,106 +1,167 @@
+<!--hero begining    -->
+
+<section id="hero">
+    <!-- hero subgrid -->
 
 
-    <!--hero begining    -->
+    <div class="wrapper">
+        <h3>Perspectives</h3>
 
-    <section id="hero">
-        <!-- hero subgrid -->
+        <h2>Great Advertising<br/> Is Art</h2>
 
-
-            <div class="wrapper">
-                <h3>Perspectives</h3>
-                <h2>Great Advertising<br/> Is Art</h2>
-                <h3>By Joe Sabia</h3>
-            </div>
+        <h3>By Joe Sabia</h3>
+    </div>
 
 
-        <!-- hero subgird end-->
-    </section>
+    <!-- hero subgird end-->
+</section>
 
-    <!--    end of hero    -->
-
-
-    <!-- ---------------------------------------------------- -->
-    <!-- ---------------------------------------------------- -->
-
-    <section class="content">
-        <div class="grid">
-
-            <div class="content-rail">
-                <p class="first">Great art inspires people.<br/>Great ads should too. </p> <?php include "../inc/social.php"; ?>
-                <p>The next time you’re at a traditional Western art museum looking at sculptures, paintings and thousands of depictions of the birth of  Jesus, consider that you’re looking at the most famous and oldest form of advertising: advertising for the Christian Church. For each gem of an ad (artwork), a company (the Church) with cash (ad dollars) commissioned artists (ad agencies) to depict and promulgate faith (ad campaigns).</p>
-                <p>Renaissance art is precious for awakening mankind’s creative soul, but who in their right mind defines Renaissance painting or sculpture as “advertising”? We say it’s “art,” right? Never mind that most masterpieces were bankrolled by deep-pocketed religious types with their own agendas.</p>
-                <p>Fast forward to ad ads. You know, the ones from Ford, Gillette, McDonalds, etc. Advertising, as opposed to art, is all about the involuntary experience of companies disrupting our space and time. Ads disrupt entertainment (TV); ads disrupt local environments (billboards); ads even disrupt the dietary habits of <a href="http://www.youtube.com/watch?v=KjB6r-HDDI0&feature=fvwrel" title="fictional characters in movies" target="blank">fictional characters in movies</a> sometimes.</p>
-
-            </div>
+<!--    end of hero    -->
 
 
+<!-- ---------------------------------------------------- -->
+<!-- ---------------------------------------------------- -->
+
+<section class="content">
+    <div class="grid">
+
+        <div class="content-rail">
+            <p class="first">Great art inspires people.<br/>Great ads should too.
+            </p> <?php include "../inc/social.php"; ?>
+            <p>The next time you’re at a traditional Western art museum looking at sculptures, paintings and thousands
+                of depictions of the birth of Jesus, consider that you’re looking at the most famous and oldest form of
+                advertising: advertising for the Christian Church. For each gem of an ad (artwork), a company (the
+                Church) with cash (ad dollars) commissioned artists (ad agencies) to depict and promulgate faith (ad
+                campaigns).</p>
+
+            <p>Renaissance art is precious for awakening mankind’s creative soul, but who in their right mind defines
+                Renaissance painting or sculpture as “advertising”? We say it’s “art,” right? Never mind that most
+                masterpieces were bankrolled by deep-pocketed religious types with their own agendas.</p>
+
+            <p>Fast forward to ad ads. You know, the ones from Ford, Gillette, McDonalds, etc. Advertising, as opposed
+                to art, is all about the involuntary experience of companies disrupting our space and time. Ads disrupt
+                entertainment (TV); ads disrupt local environments (billboards); ads even disrupt the dietary habits of
+                <a href="http://www.youtube.com/watch?v=KjB6r-HDDI0&feature=fvwrel"
+                   title="fictional characters in movies" target="blank">fictional characters in movies</a> sometimes.
+            </p>
 
         </div>
-    </section>
 
-    <section class="content remove-mobile">
-        <div class="grid">
-            <div class="strip22">
-                <div class="quote">
-                    <p>Consumers are often deeply<br/> suspicious that everyone is<br/> trying to sell them something.<br/> What’s an advertiser to do?</p>
-                </div>
+
+    </div>
+</section>
+
+<section class="content remove-mobile">
+    <div class="grid">
+        <div class="strip22">
+            <div class="quote">
+                <p>Consumers are often deeply<br/> suspicious that everyone is<br/> trying to sell them something.<br/>
+                    What’s an advertiser to do?</p>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 
+<section class="content">
+    <div class="grid">
 
-    <section class="content">
-        <div class="grid">
 
+        <div class="outbox-left">
 
-            <div class="outbox-left google">
-                <p class="caption"><i class="icon-caret-up"></i> It’s Google flashing its technology through a gorgeous music video for
+            <div class="google">
+                <p class="caption"><i class="icon-caret-up"></i> It’s Google flashing its technology through a gorgeous
+                    music video for
                     <a href="http://thewildernessdowntown.com/" title="Arcade Fire">Arcade Fire</a>.</p>
             </div>
 
-
-            <div class="content-rail">
-                <p>Renaissance art is precious for awakening mankind’s creative soul, but who in their right mind defines Renaissance painting or sculpture as “advertising”? We say it’s “art,” right? Never mind that most masterpieces were bankrolled by deep-pocketed religious types with their own agendas.</p>
-
-
-
-
-                <p>Fast forward to ad ads. You know, the ones from Ford, Gillette, McDonalds, etc. Advertising, as opposed to art, is all about the involuntary experience of companies disrupting our space and time. Ads disrupt entertainment (TV); ads disrupt local environments (billboards); ads even disrupt the dietary habits of fictional characters in movies sometimes.</p>
-                <p>But good advertising really is art. And the people who craft it well are amazing artists. That ad design you see may have been crafted by the Michelangelo of Adobe Illustrator and the copy penned by the Shakespeare of copywriting. A musical jingle you hear could be from the Beethoven of commercial scoring. Creative minds abound in advertising. These people focus their time and energy to craft something expressive and beautiful. The ads we see today just happen to have been shaped by corporate influence (the church?) and feature a logo (the cross?). </p>
-            </div>
-            <div class="outbox-right bmw remove-mobile">
-                <p class="caption"><i class="icon-sort-up"></i> It’s BMW driving cars through a <a href="http://www.youtube.com/watch?v=ZDuxWGHA-Z4&feature=player_embedded" title="giant glass apple">giant glass apple</a>.</p>
-            </div>
-        </div>
-    </section>
-
-
-    <section class="content">
-        <div class="grid">
-            <div class="outbox-left toshiba remove-mobile">
-                <p class="caption"><i class="icon-sort-up"></i> It’s Toshiba sending the <a href="http://www.youtube.com/watch?v=k6PSbUl_68k" title="first chair to space" target="blank">first chair to space</a> attached to a weather balloon.</p>
+            <div class="bmw" id="bmw_from">
+                <p class="caption"><i class="icon-sort-up"></i> It’s BMW driving cars through a <a
+                        href="http://www.youtube.com/watch?v=ZDuxWGHA-Z4&feature=player_embedded"
+                        title="giant glass apple">giant glass apple</a>.</p>
             </div>
 
-            <div class="content-rail">
-                <p>The world we live in today is <a href="http://saydaily.com/2012/06/the-problem-with-advertising-that-facebook-cant-fix.html" title="completely mediated" target="blank">completely mediated</a>. As a result, consumers are often deeply suspicious that everyone is trying to sell them something. What’s an advertiser to do? Well, it’s easy to not like ads. But it’s harder to not like good art. Any marketing department has a lot to learn from the modern-day Caravaggios and Berninis of the world, employed by ad agencies or not. The whole industry needs its own revolution in making its consumers feel inspired — not sold to.</p>
-                <p>If a company blows a consumer’s mind, the consumer will forget it’s an ad. The creator will innovate artistically. And the company? Maybe it will earn the respect that comes with the title patron.</p>
-            </div>
-        </div>
-    </section>
-
-    <section class="content bio">
-        <div class="grid">
-            <div class="content-rail">
-                <img src="_assets/adart/bio-joe.jpg" />
-                <p>
-                    <strong>Joe Sabia</strong> gave a <a href="http://blog.ted.com/2011/11/23/the-technology-of-storytelling-joe-sabia-on-ted-com/" title="TED Talk" target="blank">TED Talk</a> in 2011 on the topic of storytelling and acts as the co-curator of Boing Boing TV on all Virgin America flights.</p>
-                <a href="https://twitter.com/joesabia" class="twitter-follow-button" data-show-count="false">Follow @joesabia</a>
-
-            </div>
         </div>
 
-    </section>
+        <div id="bmw_to" class="outbox-right"></div>
+
+        <div class="content-rail">
+            <p>Renaissance art is precious for awakening mankind’s creative soul, but who in their right mind defines
+                Renaissance painting or sculpture as “advertising”? We say it’s “art,” right? Never mind that most
+                masterpieces were bankrolled by deep-pocketed religious types with their own agendas.</p>
+
+
+            <p>Fast forward to ad ads. You know, the ones from Ford, Gillette, McDonalds, etc. Advertising, as opposed
+                to art, is all about the involuntary experience of companies disrupting our space and time. Ads disrupt
+                entertainment (TV); ads disrupt local environments (billboards); ads even disrupt the dietary habits of
+                fictional characters in movies sometimes.</p>
+
+            <p>But good advertising really is art. And the people who craft it well are amazing artists. That ad design
+                you see may have been crafted by the Michelangelo of Adobe Illustrator and the copy penned by the
+                Shakespeare of copywriting. A musical jingle you hear could be from the Beethoven of commercial scoring.
+                Creative minds abound in advertising. These people focus their time and energy to craft something
+                expressive and beautiful. The ads we see today just happen to have been shaped by corporate influence
+                (the church?) and feature a logo (the cross?). </p>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="content">
+    <div class="grid">
+        <div class="outbox-left toshiba remove-mobile">
+            <p class="caption"><i class="icon-sort-up"></i> It’s Toshiba sending the <a
+                    href="http://www.youtube.com/watch?v=k6PSbUl_68k" title="first chair to space" target="blank">first
+                    chair to space</a> attached to a weather balloon.</p>
+        </div>
+
+        <div class="content-rail">
+            <p>The world we live in today is <a
+                    href="http://saydaily.com/2012/06/the-problem-with-advertising-that-facebook-cant-fix.html"
+                    title="completely mediated" target="blank">completely mediated</a>. As a result, consumers are often
+                deeply suspicious that everyone is trying to sell them something. What’s an advertiser to do? Well, it’s
+                easy to not like ads. But it’s harder to not like good art. Any marketing department has a lot to learn
+                from the modern-day Caravaggios and Berninis of the world, employed by ad agencies or not. The whole
+                industry needs its own revolution in making its consumers feel inspired — not sold to.</p>
+
+            <p>If a company blows a consumer’s mind, the consumer will forget it’s an ad. The creator will innovate
+                artistically. And the company? Maybe it will earn the respect that comes with the title patron.</p>
+        </div>
+    </div>
+</section>
+
+<section class="content bio">
+    <div class="grid">
 
 
 
+        <div class="content-rail">
+            <img src="_assets/adart/bio-joe.jpg"/>
+
+            <p>
+                <strong>Joe Sabia</strong> gave a <a
+                    href="http://blog.ted.com/2011/11/23/the-technology-of-storytelling-joe-sabia-on-ted-com/"
+                    title="TED Talk" target="blank">TED Talk</a> in 2011 on the topic of storytelling and acts as the
+                co-curator of Boing Boing TV on all Virgin America flights.</p>
+            <a href="https://twitter.com/joesabia" class="twitter-follow-button" data-show-count="false">Follow
+                @joesabia</a>
+
+        </div>
+    </div>
+
+</section>
+
+
+<script type="text/javascript">
+    // desktop replacements
+
+    $(window).resize(function() {
+        if(  $(document).width() > 1024 )  {
+            $("#bmw_to").append($('#bmw_from'));
+        };
+    });
+
+
+
+</script>
